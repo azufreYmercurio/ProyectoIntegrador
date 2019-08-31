a48552cc67703e253809bf634e07407a0e724795
azufreYmercurio <claseconpaisa@gmail.com>
2019-08-31 17:07:42 -0500

    proyectoLoco

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './ComponentesAngular/home/home.component';
 import { LoginComponent } from './ComponentesAngular/login/login.component';
-import {  DatosNuvoProComponent } from './ComponentesAngular/datos-nuvo-pro/datos-nuvo-pro.component';
+
 
 
 const routes: Routes = [
   {path: '', redirectTo: '/Home', pathMatch: 'full'},
   {path: 'Home', component: HomeComponent},
   {path: 'Login', component: LoginComponent},
-  {path: 'DatosNuvoProComponent',component:DatosNuvoProComponent}
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './ComponentesAngular/login/login.component';
 import { HomeComponent } from './ComponentesAngular/home/home.component';
-import { DatosNuvoProComponent } from './ComponentesAngular/datos-nuvo-pro/datos-nuvo-pro.component';
-import { AgregepathdeNuevosComponetesComponent } from './ComponentesAngular/agregepathde-nuevos-componetes/agregepathde-nuevos-componetes.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent,
-    DatosNuvoProComponent,
-    AgregepathdeNuevosComponetesComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
